28cf8f6ab536882bf2ffe40d1bfdad331cf30422
stickymaddness@gmail.com <stickymaddness@gmail.com@f23b34ab-6a1b-74dd-ae4f-c8c00ee5c9be>
2013-07-07 11:24:11 +0000

    Added Added filter and visitor for Curse Gems
    git-svn-id: http://procurement.googlecode.com/svn/trunk@88 f23b34ab-6a1b-74dd-ae4f-c8c00ee5c9be

---
+++ Procurement/ViewModel/ForumExportVisitors/CurseGemsVisitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using POEApi.Model;
+using Procurement.ViewModel.Filters;
+
+namespace Procurement.ViewModel.ForumExportVisitors
+{
+    internal class CurseGemsVisitor : VisitorBase
+    {
+        private const string TOKEN = "{CurseGems}";
+        public override string Visit(IEnumerable<Item> items, string current)
+        {
+            return current.Replace(TOKEN, runFilter<AuraGemsFilter>(items.OfType<Gem>().Where(g => !g.IsQuality)));
+        }
+    }
+}
