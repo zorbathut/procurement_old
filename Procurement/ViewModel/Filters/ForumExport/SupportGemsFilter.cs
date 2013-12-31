6d67e638d86df38dc1f4c005e08237ec9278174f
stickymaddness@gmail.com <stickymaddness@gmail.com@f23b34ab-6a1b-74dd-ae4f-c8c00ee5c9be>
2013-12-31 01:09:20 +0000

    Added support gem filter/visitor (Holy shit I never added this earlier?!)
    git-svn-id: http://procurement.googlecode.com/svn/trunk@159 f23b34ab-6a1b-74dd-ae4f-c8c00ee5c9be

---
+++ Procurement/ViewModel/Filters/ForumExport/SupportGemsFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    internal class SupportGemsFilter : IFilter
+    {
+        private List<string> supportGems;
+        
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Gems; }
+        }
+
+        public SupportGemsFilter()
+        {
+            supportGems = Settings.SupportGems;
+        }
+
+        public bool CanFormCategory
+        {
+            get { return true; }
+        }
+
+        public string Keyword
+        {
+            get { return "Support Gems"; }
+        }
+
+        public string Help
+        {
+            get { return "Gems that modify skill gems they are linked to"; }
+        }
+
+        public bool Applicable(Item item)
+        {
+            Gem gem = item as Gem;
+            if (gem == null)
+                return false;
+
+            return item.Properties[0].Name.Contains("Support");
+        }
+    }
+}
---
+++ Procurement/ViewModel/ForumExportVisitors/SupportGemsVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using POEApi.Model;
+using Procurement.ViewModel.Filters.ForumExport;
+
+namespace Procurement.ViewModel.ForumExportVisitors
+{
+    internal class SupportGemsVisitor : VisitorBase
+    {
+        private const string TOKEN = "{SupportGems}";
+        public override string Visit(IEnumerable<Item> items, string current)
+        {
+            return current.Replace(TOKEN, runFilter<SupportGemsFilter>(items.OfType<Gem>().Where(g => !g.IsQuality).OrderBy(g => g.IconURL)));
+        }
+    }
+}
